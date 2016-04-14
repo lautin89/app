b0c9a1b4afb31ad04ecf8a08faadd2596220cc51
jason <1538731090@qq.com>
2016-04-14 20:33:56 +0800

    the seconde time to commit

--- index.php
+++ index.php
@@ -16,8 +16,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',FALSE);
-
-// 定义应用目录
+//这是第一次添加的注释
+//这是第二次添加的注释
+//定w义应用目录
 define('APP_PATH','./APP/');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
